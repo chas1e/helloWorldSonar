5f3e813d0cf7abbdf0ca5d239e15621e4d90c2db
Chaste <pew.monstaaaa@gmail.com>
2021-03-12 11:31:23 +0100

    ijdoaisjdoijoidsaaflkjdf

--- src/main/java/ch/noser/uek223/core/security/SecurityConfiguration.java
+++ src/main/java/ch/noser/uek223/core/security/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/v3/api-docs/**", "/webjars/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
