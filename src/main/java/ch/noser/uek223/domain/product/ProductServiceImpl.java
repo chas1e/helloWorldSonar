c20a89934a88040cb870ce9086186b6eb1dff1e6
Chaste <pew.monstaaaa@gmail.com>
2021-03-12 08:59:45 +0100

    Hallo

--- src/main/java/ch/noser/uek223/domain/product/ProductServiceImpl.java
+++ src/main/java/ch/noser/uek223/domain/product/ProductServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService{
-
+//Commit
     private ProductRepository productRepository;
 
     @Autowired
